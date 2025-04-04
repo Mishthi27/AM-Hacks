f3dc9e7068d544b2b5be8509094108b177e8eff9
advika31 <advikasinghal31@gmail.com>
2025-04-05 00:20:28 +0530

    Update App.js

--- frontend/retentionx/src/App.js
+++ frontend/retentionx/src/App.js
@@ -4,11 +4,11 @@ import LandingPage from "./components/landing/landing";
 import LoginPage from "./components/signup/login"; 
 import Dashboard from "./components/dashboard/Dashboard";
 import FlashcardList from "./components/flashcard/flashcardlist";
-// import Quiz from "./components/quiz/Quiz";
-// import Progress from "./components/progress/progress";
-// import PomodoroTimer from "./components/pomodoro timer/pomodoroTimer";
-// import UploadNotes from "./components/uploadNotes/uploadNotes";
-// import RevisedNotes from "./components/reviseNotes/reviseNotes";
+import Quiz from "./components/quiz/Quiz";
+import Progress from "./components/progress/progress";
+import PomodoroTimer from "./components/pomodoro timer/pomodoroTimer";
+import UploadNotes from "./components/uploadNotes/uploadNotes";
+import RevisedNotes from "./components/reviseNotes/reviseNotes";
 
 import { useEffect } from "react";
 
@@ -20,11 +20,11 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/Dashboard" element={<Dashboard/>}/>
         <Route path="/flashcardlist" element={<FlashcardList />} />
-        {/* <Route path="/quiz" element={<Quiz />} /> */}
-        {/* <Route path="/progress" element={<Progress/>}/> */}
-        {/* <Route path="/pomodoroTimer" element={<PomodoroTimer/>}/> */}
-        {/* <Route path="/uploadNotes" element={<UploadNotes/>} /> */}
-        {/* <Route path="/reviseNotes" element={<RevisedNotes/>} /> */}
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/progress" element={<Progress/>}/>
+        <Route path="/pomodoroTimer" element={<PomodoroTimer/>}/>
+        <Route path="/uploadNotes" element={<UploadNotes/>} />
+        <Route path="/reviseNotes" element={<RevisedNotes/>} />
 
       </Routes>
     </Router>

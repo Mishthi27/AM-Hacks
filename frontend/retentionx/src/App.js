0a4cb8e0f98b87464eed3a29421a9c38a4a237cd
advika31 <advikasinghal31@gmail.com>
2025-04-04 17:10:22 +0530

    Update App.js

--- frontend/retentionx/src/App.js
+++ frontend/retentionx/src/App.js
@@ -32,18 +32,18 @@ function App() {
 {/* <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
 <script src="https://files.bpcontent.cloud/2025/02/21/17/20250221173635-6DOLLHY3.js"></script> */}
     
-// useEffect(() => {
-//   const script = document.createElement("script");
-//   script.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
-//   script.src =
-//     "https://files.bpcontent.cloud/2025/02/21/17/20250221173635-6DOLLHY3.js";
-//   script.async = true;
-//   document.body.appendChild(script);
+useEffect(() => {
+  const script = document.createElement("script");
+  script.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
+  script.src =
+    "https://files.bpcontent.cloud/2025/02/21/17/20250221173635-6DOLLHY3.js";
+  script.async = true;
+  document.body.appendChild(script);
   
-//   return () => {
-//     document.body.removeChild(script); 
-//   };
-// }, []);
+  return () => {
+    document.body.removeChild(script); 
+  };
+}, []);
 }
 
 export default App;
